10ee84f2727d1831bbbd73d6d5b7cd027eedebca
yzoug <zoug@protonmail.ch>
2020-05-06 20:47:17 +0200

    SQLAlchemy models for a lobby, card, player

--- app/models.py
+++ app/models.py
@@ -1,37 +1,65 @@
 from app import db
 from datetime import datetime
 
+played_cards_in_lobbies = db.Table('played_cards_in_lobbies',
+        db.Model.metadata,
+        db.Column('card_id', db.Integer, db.ForeignKey('card.id')),
+        db.Column('lobby_id', db.Integer, db.ForeignKey('lobby.id'))
+    )
+
 class Card(db.Model):
+    # id
     id = db.Column(db.Integer, primary_key=True)
+
+    # data
+    value = db.Column(db.Integer, nullable=False)
+    real_value = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.Integer, nullable=False)
+    image_name = db.Column(db.String(50), nullable=False)
+
+    # foreign keys
+    # many to one
+    owner = db.relationship("Player", back_populates="cards")
     owner_id = db.Column(db.Integer, db.ForeignKey("player.id"))
-    value = db.Column(db.Integer)
-    real_value = db.Column(db.Integer)
-    color = db.Column(db.Integer)
 
 class Player(db.Model):
+    # id
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    number_of_lives = db.Column(db.Integer)
-    lobby_id = db.Column(db.Integer, db.ForeignKey("lobby.id"))
-    cards = db.relationship("Card", backref="owner_id")
+
+    # data
+    name = db.Column(db.String(50), nullable=False)
+    number_of_lives = db.Column(db.Integer, nullable=False)
     current_guess = db.Column(db.Integer)
     current_wins = db.Column(db.Integer)
+    is_playing = db.Column(db.Boolean)
+    is_dealer = db.Column(db.Boolean)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    # foreign keys
+    # many to one
+    lobby = db.relationship("Lobby", back_populates="players")
+    lobby_id = db.Column(db.Integer, db.ForeignKey("lobby.id"))
+    # one to many
+    cards = db.relationship("Card", back_populates="owner")
 
 class Lobby(db.Model):
+    # id
     id = db.Column(db.Integer, primary_key=True)
-    creator_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+
+    # data
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     gameturn = db.Column(db.Integer, default=0)
     turn = db.Column(db.Integer)
-    players = db.relationship("Player", backref="lobby")
-    current_dealer_id = db.Column(db.Integer, db.ForeignKey("player.player_id"))
-    current_player_id = db.Column(db.Integer, db.ForeignKey("player.player_id"))
-    beforemanilla = db.Column(db.Integer, db.ForeignKey('card.id'))
     number_of_turns = db.Column(db.Integer)
     turn_type = db.Column(db.Integer)
     current_win_value = db.Column(db.Integer, default=1)
-    played_cards = db.relationship("Card", backref="lobby_id")
+
+    # foreign keys
+    # one to many
+    players = db.relationship("Player", back_populates="lobby")
+    # many to one
+    beforemanilla = db.Column(db.Integer, db.ForeignKey("card.id"))
+    before_manilla = db.relationship("Card")
+    before_manilla_id = db.Column(db.Integer, db.ForeignKey("lobby.id"))
+    # many to many
+    played_cards = db.relationship("Card", secondary=played_cards_in_lobbies)
 
